f1c046081e9e1aa6040c4dbd86877d3afc5128a6
Claudio Chies <claudio@chies.com>
2025-02-04 14:10:46 +0100

    Rename ESPG to EPSG, and Add support for non-CH1903 Geotiffs

--- grid_map_geo/include/grid_map_geo/transform.h
+++ grid_map_geo/include/grid_map_geo/transform.h
@@ -31,66 +31,66 @@
  *
  ****************************************************************************/
 
-#ifndef GRID_MAP_GEO_TRANSFORM_H
-#define GRID_MAP_GEO_TRANSFORM_H
+ #ifndef GRID_MAP_GEO_TRANSFORM_H
+ #define GRID_MAP_GEO_TRANSFORM_H
 
-#include <gdal/gdal.h>
-#include <gdal/gdal_priv.h>
-#include <gdal/ogr_p.h>
-#include <gdal/ogr_spatialref.h>
+ #include <gdal/gdal.h>
+ #include <gdal/gdal_priv.h>
+ #include <gdal/ogr_p.h>
+ #include <gdal/ogr_spatialref.h>
 
-#include <Eigen/Dense>
+ #include <Eigen/Dense>
 
-enum class ESPG { ECEF = 4978, WGS84 = 4326, WGS84_32N = 32632, CH1903_LV03 = 21781 };
+ enum class EPSG { ECEF = 4978, WGS84 = 4326, WGS84_32N = 32632, CH1903_LV03 = 21781 };
 
-/**
- * @brief Helper function for transforming using gdal
- *
- * @param src_coord
- * @param tgt_coord
- * @param source_coordinates
- * @return Eigen::Vector3d
- */
-inline Eigen::Vector3d transformCoordinates(ESPG src_coord, ESPG tgt_coord, const Eigen::Vector3d source_coordinates) {
-  OGRSpatialReference source, target;
-  source.importFromEPSG(static_cast<int>(src_coord));
-  target.importFromEPSG(static_cast<int>(tgt_coord));
+ /**
+  * @brief Helper function for transforming using gdal
+  *
+  * @param src_coord
+  * @param tgt_coord
+  * @param source_coordinates
+  * @return Eigen::Vector3d
+  */
+ inline Eigen::Vector3d transformCoordinates(EPSG src_coord, EPSG tgt_coord, const Eigen::Vector3d source_coordinates) {
+   OGRSpatialReference source, target;
+   source.importFromEPSG(static_cast<int>(src_coord));
+   target.importFromEPSG(static_cast<int>(tgt_coord));
 
-  OGRPoint p;
-  p.setX(source_coordinates(0));
-  p.setY(source_coordinates(1));
-  p.setZ(source_coordinates(2));
-  p.assignSpatialReference(&source);
+   OGRPoint p;
+   p.setX(source_coordinates(0));
+   p.setY(source_coordinates(1));
+   p.setZ(source_coordinates(2));
+   p.assignSpatialReference(&source);
 
-  p.transformTo(&target);
-  Eigen::Vector3d target_coordinates(p.getX(), p.getY(), p.getZ());
-  return target_coordinates;
-}
+   p.transformTo(&target);
+   Eigen::Vector3d target_coordinates(p.getX(), p.getY(), p.getZ());
+   return target_coordinates;
+ }
 
-/**
- * @brief Helper function for transforming using gdal
- *
- * @param src_coord
- * @param wkt
- * @param source_coordinates
- * @return Eigen::Vector3d
- */
-inline Eigen::Vector3d transformCoordinates(ESPG src_coord, const std::string wkt,
-                                            const Eigen::Vector3d source_coordinates) {
-  OGRSpatialReference source, target;
-  char* wkt_string = const_cast<char*>(wkt.c_str());
-  source.importFromEPSG(static_cast<int>(src_coord));
-  target.importFromWkt(&wkt_string);
+ /**
+  * @brief Helper function for transforming using gdal
+  *
+  * @param src_coord
+  * @param wkt
+  * @param source_coordinates
+  * @return Eigen::Vector3d
+  */
+ inline Eigen::Vector3d transformCoordinates(EPSG src_coord, const std::string wkt,
+                                             const Eigen::Vector3d source_coordinates) {
+   OGRSpatialReference source, target;
+   char* wkt_string = const_cast<char*>(wkt.c_str());
+   source.importFromEPSG(static_cast<int>(src_coord));
+   target.importFromWkt(&wkt_string);
 
-  OGRPoint p;
-  p.setX(source_coordinates(0));
-  p.setY(source_coordinates(1));
-  p.setZ(source_coordinates(2));
-  p.assignSpatialReference(&source);
+   OGRPoint p;
+   p.setX(source_coordinates(0));
+   p.setY(source_coordinates(1));
+   p.setZ(source_coordinates(2));
+   p.assignSpatialReference(&source);
 
-  p.transformTo(&target);
-  Eigen::Vector3d target_coordinates(p.getX(), p.getY(), p.getZ());
-  return target_coordinates;
-}
+   p.transformTo(&target);
+   Eigen::Vector3d target_coordinates(p.getX(), p.getY(), p.getZ());
+   return target_coordinates;
+ }
 
-#endif
+ #endif
--- grid_map_geo/src/grid_map_geo.cpp
+++ grid_map_geo/src/grid_map_geo.cpp
@@ -85,6 +85,8 @@ bool GridMapGeo::initializeFromGeotiff(const std::string &path) {
 
   const OGRSpatialReference *spatial_ref = dataset->GetSpatialRef();
   std::string name_coordinate = spatial_ref->GetAttrValue("geogcs");
+  std::string epsg_code = spatial_ref->GetAttrValue("AUTHORITY", 1);
+
   // Get image metadata
   unsigned width = dataset->GetRasterXSize();
   unsigned height = dataset->GetRasterYSize();
@@ -99,7 +101,7 @@ bool GridMapGeo::initializeFromGeotiff(const std::string &path) {
 
   double mapcenter_e = originX + pixelSizeX * width * 0.5;
   double mapcenter_n = originY + pixelSizeY * height * 0.5;
-  maporigin_.espg = ESPG::CH1903_LV03;
+  maporigin_.epsg = static_cast<EPSG>(std::stoi(epsg_code));
   maporigin_.position = Eigen::Vector3d(mapcenter_e, mapcenter_n, 0.0);
 
   Eigen::Vector2d position{Eigen::Vector2d::Zero()};
@@ -184,7 +186,7 @@ bool GridMapGeo::initializeFromVrt(const std::string &path, const Eigen::Vector2
   grid_map::Length length(lengthX, lengthY);
   std::cout << "length: " << length.transpose() << std::endl;
 
-  maporigin_.espg = static_cast<ESPG>(std::stoi(epsg_code));
+  maporigin_.epsg = static_cast<EPSG>(std::stoi(epsg_code));
   maporigin_.position = map_center.head(2);
 
   Eigen::Vector2d position{Eigen::Vector2d::Zero()};
@@ -269,7 +271,6 @@ bool GridMapGeo::addColorFromGeotiff(const std::string &path) {
   const double lengthX = resolution * width;
   const double lengthY = resolution * height;
   grid_map::Length length(lengthX, lengthY);
-
   grid_map_.add("color");
   GDALRasterBand *raster_red = dataset->GetRasterBand(1);
   GDALRasterBand *raster_green = dataset->GetRasterBand(2);
@@ -414,9 +415,9 @@ bool GridMapGeo::AddLayerOffset(const double offset_distance, const std::string 
   return true;
 }
 
-void GridMapGeo::setGlobalOrigin(ESPG src_coord, const Eigen::Vector3d origin) {
+void GridMapGeo::setGlobalOrigin(EPSG src_coord, const Eigen::Vector3d origin) {
   // Transform global origin into CH1903 / LV03 coordinates
-  maporigin_.espg = src_coord;
+  maporigin_.epsg = src_coord;
   maporigin_.position = origin;
 }
 
